ac93313767d4faf0a715d2ccbef4f8733718433f
Samuel Banya <sbanya@fastmail.com>
2022-05-02 19:51:33 -0400

    commit tree 6d4861d7b999aa9459bf8d4df66290107da02f46

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function superbowlWin(objectsArray) {
+    // let foundItem = books.find(book => book.title === 'Javascript: The Definitive Guide');
+    const foundObject = objectsArray.find(object => object.result === 'W');
+
+    if (foundObject) {
+        return foundObject.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2018", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+]
+
+superbowlWin(record);
